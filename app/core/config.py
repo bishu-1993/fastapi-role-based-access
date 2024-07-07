fb64b2022e0f9f320f0857e842160096ce28b295
bishwajeet <bishu1993@gmail.com>
2024-07-07 22:42:29 +0530

    secure redis data

--- app/core/config.py
+++ app/core/config.py
@@ -2,9 +2,12 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
+from cryptography.fernet import Fernet
+import base64
 
 class Settings:
+    key = Fernet.generate_key()
+    cipher_suite = Fernet(key)
     DATABASE_URL:str = os.getenv("DATABASE_URL")
     JWT_EXPIRATION_SECONDS: int = 60000
     REDIS_URL: str = os.getenv("REDIS_URL")
--- app/core/redis.py
+++ app/core/redis.py
@@ -2,6 +2,7 @@ import aioredis
 from datetime import timedelta
 from app.core.config import settings
 
+
 redis = None
 
 async def get_redis():
@@ -12,16 +13,27 @@ async def get_redis():
 
 async def set_jwt_token(token: str, username: str):
     try:
+        token_encrypted = settings.cipher_suite.encrypt(token.encode())
         redis = await get_redis()
-        await redis.setex(f"token:{token}", timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES), username)
+        await redis.setex(f"token:{username}", timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES), token_encrypted)
     except aioredis.RedisError as e:
         print(f"Redis error occurred: {e}")
     finally:
         if redis:
             await redis.close()
 
-async def get_user_id_from_token(token: str):
-    redis = await get_redis()
-    user_id = await redis.get(f"token:{token}")
-    return int(user_id) if user_id else None
-
+async def get_token_for_user(username: str):
+    try:
+        redis = await get_redis()
+        token_encrypted = await redis.get(f"token:{username}")
+        if token_encrypted:
+            # Decrypt the token
+            token_decrypted = settings.cipher_suite.decrypt(token_encrypted).decode('utf-8')
+            return token_decrypted
+        return None
+    except aioredis.RedisError as e:
+        print(f"Redis error occurred: {e}")
+        return None
+    finally:
+        if redis:
+            await redis.close()
